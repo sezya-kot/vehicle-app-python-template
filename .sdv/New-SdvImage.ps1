12fd641ab97bdcd45a59fcb9c8296147e52d5a18
Fabian Lohauß <fabianl@microsoft.com>
2021-11-02 08:54:31 +0000

    refactoring

--- .sdv/New-SdvImage.ps1
+++ .sdv/New-SdvImage.ps1
@@ -1,10 +1,18 @@
 # docker login ghcr.io 
 
-docker build -t ghcr.io/softwaredefinedvehicle/vehicle-app-python-template/client_base_amd64 -f src/Dockerfile.base .
-docker push ghcr.io/softwaredefinedvehicle/vehicle-app-python-template/client_base_amd64
+Import-Module $PSScriptRoot/Sdv.psm1 -Force
+
+foreach($Component in (Find-SdvVehicleApp | Get-SdvComponent)) {
+    $ImageName = ("{0}_base_amd64" -f $Component.Name)
+    $Dockerfile = Join-Path $Component.Folder -ChildPath "Dockerfile.base"
+    $DockerContext = $Component.DockerFolder
+    docker build -t ghcr.io/softwaredefinedvehicle/vehicle-app-python-template/$ImageName -f $Dockerfile $DockerContext
+    docker push ghcr.io/softwaredefinedvehicle/vehicle-app-python-template/$ImageName
+}
 
 
-docker build -t ghcr.io/softwaredefinedvehicle/vehicle-app-python-template/vehicleapi_base_amd64 -f vehicleapi/Dockerfile.base ./vehicleapi
-docker push ghcr.io/softwaredefinedvehicle/vehicle-app-python-template/vehicleapi_base_amd64
+
+# docker build -t ghcr.io/softwaredefinedvehicle/vehicle-app-python-template/vehicleapi_base_amd64 -f vehicleapi/Dockerfile.base ./vehicleapi
+# docker push ghcr.io/softwaredefinedvehicle/vehicle-app-python-template/vehicleapi_base_amd64
 
 # docker build -t vehicleapi -f vehicleapi/Dockerfile .
