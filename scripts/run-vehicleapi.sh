b908fa266a1c53bb4babead123a3da8f7771c3f3
Ashwin Adarsh <40774798+Psychotic-Coder@users.noreply.github.com>
2022-04-06 15:40:05 +0530

    AB#8517 Remove vehicleapi-mock References (#112)
    * Cleanup
    
    * Remove comments

--- scripts/run-vehicleapi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PKG_PATH=$(python -c 'import os,inspect,sdv; print(os.path.dirname(inspect.getfile(sdv)))')
-
-
-dapr run --app-id vehicleapi --app-protocol grpc --app-port 50051 --components-path ./.dapr/components --config ./.dapr/config.yaml \
-python3 $PKG_PATH/vehicle_api_mock/vehicleapi.py
